1d6128ae0a37e5c247d1d8c1dd565cce1718c122
Daniel Chernov <chernov.dani@gmail.com>
2026-06-22 11:23:59 +0300

    Update search-controller.js

--- js/controllers/search-controller.js
+++ js/controllers/search-controller.js
@@ -1,26 +1,39 @@
 const SearchController = function (model, renderer) {
-    const handleSearch = async function(){
+    const handleSearch = async function () {
         renderer.clearErrors()
         renderer.clearResults()
 
-        const quary = renderer.getInputValue()
-        if (quary==="") {
+        const query = renderer.getInputValue()
+
+        if (!query) {
             renderer.showError("Please enter company name or symbol")
             return
         }
+
         renderer.showLoading()
-        const response = await model.searchCompanies(quary)
-        if (!response.result){
+
+        const response = await model.searchCompanies(query)
+
+        renderer.hideLoading()
+
+        if (!response.result) {
             renderer.showError(response.message)
             return
         }
-        renderer.hideLoading()
+
+        if (response.data.length === 0) {
+            renderer.showError("No companies found")
+            return
+        }
+
         renderer.renderCompanies(response.data)
     }
+
     const init = function () {
         renderer.onSearch(handleSearch)
     }
-    return{
+
+    return {
         init
     }
 }
